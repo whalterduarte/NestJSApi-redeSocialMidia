1c0e474d5a1488baa2ef4500a43d132158ff3b63
Whalter Duarte <whalterdev@gmail.com>
2024-06-02 14:45:54 -0300

    :bug: - create folder public

--- src/modules/upload/post-upload/post-upload.service.ts
+++ src/modules/upload/post-upload/post-upload.service.ts
@@ -1,21 +1,33 @@
-import { Injectable } from '@nestjs/common';
-import { MulterFile } from 'multer';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { extname } from 'path';
+import { v4 as uuidv4 } from 'uuid';
+import * as sharp from 'sharp';
 import * as path from 'path';
 import * as fs from 'fs';
-import { v4 as uuidv4 } from 'uuid';
+import { MulterFile } from 'multer';
 
 @Injectable()
 export class PostUploadService {
   async savePostFile(file: MulterFile): Promise<string> {
     try {
-      if (!file || !file.originalname) {
-        throw new Error('Nenhum arquivo enviado');
+      const directoryPath = path.join(__dirname, '../../../../public/uploads/posts');
+
+      // Verificar se o diretório existe, senão criar
+      if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true });
       }
 
-      const uniqueFileName = uuidv4() + path.extname(file.originalname);
-      const filePath = path.join(__dirname, '../../../../public/uploads/posts', uniqueFileName);
-      
-      await fs.promises.writeFile(filePath, file.buffer);
+      if (!file || !file.originalname) {
+        throw new BadRequestException('Nenhum arquivo enviado');
+      }
+
+      const uniqueFileName = uuidv4() + extname(file.originalname);
+      const filePath = path.join(directoryPath, uniqueFileName);
+
+      await sharp(file.buffer)
+        .resize(1024, 1024)
+        .jpeg({ quality: 80 })
+        .toFile(filePath);
       const url = `${process.env.BASE_API}/uploads/posts/${uniqueFileName}`;
 
       return url;
