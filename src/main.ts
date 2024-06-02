be6c5e145725decd1f8dcd794cf4ae1e7f927359
Whalter Duarte <whalterdev@gmail.com>
2024-06-02 15:34:44 -0300

    :sparkles: - Add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ require('dotenv').config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+  app.enableCors(
+   {
+    origin: `${process.env.BASE_FRONT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+   }
+  );
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
